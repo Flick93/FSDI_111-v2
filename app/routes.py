0a378278c801a902029548794f113e15f788327a
Flick93 <p.afflick@outlook.com>
2024-10-20 23:35:53 -0700

    final assignment
    Front end task mgr

--- app/routes.py
+++ app/routes.py
@@ -1,45 +1,99 @@
 from flask import (
     Flask,
-    request
+    render_template,   #alias for requests context object
+    request as req
 )
 
-from app.database import task
+import requests       #this is the standalone request python package
+
+BACKEND_URL = "http://127.0.0.1:5000/tasks"
 
 app = Flask(__name__)
-#Rest: an architectural design pattern for building connected ssytems.
-#Name endpoints after plural known ex: ("Orders/Tasks/Lists")
-#PK = Primary Key
+
+@app.get("/")
+def index():
+    return render_template("index.html")
+
 @app.get("/tasks")
-def get_all_tasks():
-    tasks = task.scan()
-    out = {
-        "tasks": tasks,
-        "ok":True
-    }
-    return out
+def view_tasks():
+    response = requests.get(BACKEND_URL)
+    if response.status_code == 200:
+        task_list = response.json().get("tasks") #json turned into python dictionary
+        return render_template("list.html", tasks=task_list)
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
 
 @app.get("/tasks/<int:pk>/")
-def get_task_by_id(pk):
-    single_task = task.select_by_id(pk)
-    out = {
-        "tasks": single_task,
-        "ok":True
-    }
-    return out
+def single_task(pk):
+    url = "%s/%s" % (BACKEND_URL, pk) #means of formatting a string
+    response = requests.get(url)
+    if response.status_code == 200:
+        task_data = response.json().get("tasks") 
+        return render_template("detail.html", tasks=task_data)
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
 
-@app.post("/tasks")
-def create_tasks():
-    task_data = request.json
-    task.insert(task_data)
-    return "", 204
+@app.get("/tasks/new/")
+def new_form():
+    return render_template("new.html")
 
-@app.put("/tasks/<int:pk>/")
-def update_task_by_id(pk):
-    task_data = request.json
-    task.update_by_id(task_data, pk)
-    return "",204
+@app.post("/tasks/new/")
+def create_form():
+    task_data = req.json
+    response = requests.post(BACKEND_URL, task_data)
+    if response.status_code == 204:
+        return render_template("success.html", message="Task Created")
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
 
-@app.delete("/tasks/<int:pk>/")
-def delete_task_by_id(pk):
-    task.delete_by_id(pk)
-    return "",204
+@app.get("/tasks/<int:pk>/edit/")
+def edit_form(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.get(url)
+    if response.status_code == 200:
+        task_data = response.json().get("task") 
+        return render_template("edit.html", task=task_data)
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
+
+@app.post("/tasks/<int:pk>/edit/")
+def edit_task(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.put(url, json=req.json)
+    if response.status_code == 204:
+        return render_template("success.html", message="Task Edited")
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
+
+@app.get("/tasks/<int:pk>/delete/")
+def delete_form(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.get(url)
+    if response.status_code == 200:
+        task_data = response.json().get("task") 
+        return render_template("delete.html", task=task_data)
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
+
+@app.post("/tasks/<int:pk>/delete/")
+def delete_task(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.put(url)
+    if response.status_code == 204:
+        return render_template("success.html", message="Task Deleted")
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
